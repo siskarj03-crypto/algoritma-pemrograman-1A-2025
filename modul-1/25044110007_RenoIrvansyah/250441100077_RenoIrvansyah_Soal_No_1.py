2e33d88b0869c5372ab5b1c4b2ec6c13229de43f
syrnz <irvansyah.reno@gmail.com>
2025-09-27 12:22:38 +0700

    250441100077_RenoIrvansyah_ALPRO_Modul1

---
+++ modul-1/25044110007_RenoIrvansyah/250441100077_RenoIrvansyah_Soal_No_1.py
@@ -0,0 +1,8 @@
+buku_tulis = 5000
+harga_pensil = 4500
+pajak_pembelian = 0.1
+
+total_harga = (buku_tulis * 3) + (harga_pensil * 2)
+harga_setelah_pajak = total_harga + (total_harga * pajak_pembelian)
+print("Harga total pembelian adalah:", total_harga)
+print("Total harga setelah pajak adalah:", harga_setelah_pajak)
