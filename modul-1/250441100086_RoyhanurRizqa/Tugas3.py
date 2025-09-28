691f265a95e4ca9aaa6f7ad7908ea1887e4988d0
Royhanur Rizqa <royhanur123@gmail.com>
2025-09-28 21:38:01 +0700

    250441100086_RoyhanurRizqa_modul1_AsprakKakDestya

--- modul-1/250441100086_RoyhanurRizqa/Tugas3.py
+++ modul-1/250441100086_RoyhanurRizqa/Tugas3.py
@@ -1,7 +1,7 @@
 print("Dalam sebuah kotak terdapat 8 bola merah dan 6 bola biru. Seorang anak akan mengambil 3 bola sekaligus secara acak.")
 print("berapa banyak kemungkinan kombinasi bola yang dapat diambil!")
 
-
+import math
 
 total_bola=8+6
 bola_yang_diambil=3
