9ace72e2dc6c4c07e34cbac4009fbbc077dd6ce8
julian <jku8765@gmail.com>
2025-09-29 00:13:53 +0700

    25044100012_JuliansahkhoirulUmam_modul1_asprakkakyahya

---
+++ modul-1/250441100012_JuliansahKhoirulUmam/tugas1.py
@@ -0,0 +1,7 @@
+buku = 5000 * 3 # Di Soal Halim membeli 3 buku
+pensil = 4500 * 2 # Di soal Halim Membeli 2 Buku
+harga = buku + pensil 
+print("Total Harga yang Harus Di Bayar Sebelum Mendapat Pajak : ",harga)
+pajak = harga * 0.10 # Pajak 10% dari 24.000 Adalah 10/100 dari 24000 : 2.400
+total_harga = harga + pajak 
+print("Total Harga yang Harus Di Bayar Setelah Mendapat Pajak : ",total_harga)
---
+++ modul-1/250441100012_JuliansahKhoirulUmam/tugas2.py
@@ -0,0 +1,7 @@
+panjang = int(input("Masukkan Nilai Panjang : ")) # Di soal Panjang 1o cm
+lebar = int(input("Masukkan Nilai Lebar : ")) # Di soal Lebar 6 cm
+Tinggi = int(input("Masukkan Nilai Tinggi : ")) # Di soal Tinggi 4 cm
+volume = panjang * lebar * Tinggi # Rumus Volume Adalaah p*l*t
+luas = 2*(panjang*lebar + panjang*Tinggi + lebar*Tinggi) # rumus Luas Adalah 2x(p*l*t)
+print("Volume Dari Balok Tersebut Adalah : ",volume)
+print("Luas Dari Balok Tersebut Adalah : ",luas)
