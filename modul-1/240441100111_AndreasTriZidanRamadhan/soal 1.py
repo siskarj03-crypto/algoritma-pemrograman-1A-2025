bb89cb8b7029b81a4fb1fc35bdd2192d9d82afab
andreaszidan <andreaszidan110@gmail.com>
2025-09-29 20:47:31 +0700

    240441100111_AndreasTriZidanRamadhan_Modul1_KakYahya

---
+++ modul-1/240441100111_AndreasTriZidanRamadhan/soal 1.py
@@ -0,0 +1,12 @@
+buku_tulis = 5000
+bulpen = 4500
+total_buku = buku_tulis * 3
+total_bulpen = bulpen * 2
+
+total_harga = total_buku + total_bulpen
+pajak = total_harga*0.1
+
+akhir= total_harga - pajak
+
+print("Total belanja hallim: ",total_harga)
+print("Jadi total belanja yang hallim bayar setelah pajak: ",akhir)
---
+++ modul-1/240441100111_AndreasTriZidanRamadhan/soal 2.py
@@ -0,0 +1,8 @@
+panjang = int(input("Masukkan panjang: "))
+lebar = int(input("Masukkan lebar: "))
+tinggi = int(input("Masukkan tinggi: "))
+
+volume = panjang * lebar * tinggi
+luas= 2*((panjang*lebar)+(panjang*tinggi)+(lebar*tinggi))
+print("Jadi volume dari balok tersebut adalah ",volume)
+print("Jadi luas permukaan dari balok tersebut adalah ",luas)
