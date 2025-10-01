e5ad6038e186994d99eeedd176384df130bca043
250441100106crysolitiara <crysolisimanjuntak@gmail.com>
2025-10-01 17:18:12 +0700

    250441100106_CrysoliTiaraSimanjuntak_Modul1_AsprakDestya

---
+++ modul-1/250441100106_CrysoliTiaraSimanjuntak_AsprakDestya/250441100106_CrysoliTiaraSimanjuntak_SoalNo3.py
@@ -0,0 +1,12 @@
+# jumlah bola merah dan biru
+merah = 8
+biru = 6
+total = merah + biru
+
+# jumlah bola yang diambil
+ambil = 3
+
+# hitung kombinasi
+kombinasi = total * (total -1) * (total - 2) // 6
+
+print("Jumlah kemungkinan kombinasi bola yang bisa diambil adalah:" , kombinasi)
