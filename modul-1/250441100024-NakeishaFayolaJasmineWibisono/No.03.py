695e9c56109fbf32d0cfc670af10beb92161e283
fayola3007-png <fayola3007@gmail.com>
2025-10-06 20:58:16 +0700

    250441100024-NakeishaFayolaJasmineWibisono-modul1&2-AsprakKakYahya

---
+++ modul-1/250441100024-NakeishaFayolaJasmineWibisono/No.03.py
@@ -0,0 +1,12 @@
+#variabel jumlah bola
+merah = 8
+biru = 6
+total = merah + biru
+ambil = 3
+
+memungkinkan = total * (total -1) * (total-2) / (ambil * (ambil - 1) * (ambil - 2))
+print ("banyaknya kombinasi bola yang memungkinkan dapat diambil adalah:", memungkinkan)
+print (" ")
+
+
+
